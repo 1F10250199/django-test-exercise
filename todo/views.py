cf5018ead18ea763188384349177095ef3030236
1F10250258 <s1F102502586@iniad.org>
2026-07-22 22:21:56 +0900

    タスクのタイトル検索機能を追加

--- todo/views.py
+++ todo/views.py
@@ -10,13 +10,21 @@ def index(request):
         task = Task(title=request.POST['title'],
                     due_at=make_aware(parse_datetime(request.POST['due_at'])))
         task.save()
+    
+    tasks = Task.objects.all()
+
+    keyword = request.GET.get('keyword')
+    if keyword:
+        tasks = tasks.filter(title__icontains=keyword)
+
     if request.GET.get('order') == 'due':
-        tasks = Task.objects.order_by('due_at')
+        tasks = tasks.order_by('due_at')
     else:
-        tasks = Task.objects.order_by('-posted_at')
+        tasks = tasks.order_by('-posted_at')
 
     context = {
-        'tasks': tasks
+        'tasks': tasks,
+        'keyword': keyword,  
     }
     return render(request, 'todo/index.html', context)
 
